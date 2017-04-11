f45ca369e4bdbadfba2404b9ce6a18e79e50c4e7
Milco Numan <milco.numan@gmail.com>
2017-04-11 13:12:51 +0200

    4.3.1 finished, scopes; required explicit end() invocation on error message

--- exercises/ch-4-ex-2/protectedResource.js
+++ exercises/ch-4-ex-2/protectedResource.js
@@ -7,7 +7,9 @@ var cors = require('cors');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support form-encoded bodies (for bearer tokens)
 
 app.engine('html', cons.underscore);
 app.set('view engine', 'html');
@@ -18,76 +20,88 @@ app.use('/', express.static('files/protectedResource'));
 app.use(cors());
 
 var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+    "name": "Protected Resource",
+    "description": "This data has been protected by OAuth 2.0"
 };
 
 var getAccessToken = function(req, res, next) {
-	var inToken = null;
-	var auth = req.headers['authorization'];
-	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
-		inToken = auth.slice('bearer '.length);
-	} else if (req.body && req.body.access_token) {
-		inToken = req.body.access_token;
-	} else if (req.query && req.query.access_token) {
-		inToken = req.query.access_token
-	}
-	
-	console.log('Incoming token: %s', inToken);
-	nosql.one(function(token) {
-		if (token.access_token == inToken) {
-			return token;	
-		}
-	}, function(err, token) {
-		if (token) {
-			console.log("We found a matching token: %s", inToken);
-		} else {
-			console.log('No matching token was found.');
-		}
-		req.access_token = token;
-		next();
-		return;
-	});
+    var inToken = null;
+    var auth = req.headers['authorization'];
+    if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
+        inToken = auth.slice('bearer '.length);
+    } else if (req.body && req.body.access_token) {
+        inToken = req.body.access_token;
+    } else if (req.query && req.query.access_token) {
+        inToken = req.query.access_token
+    }
+
+    console.log('Incoming token: %s', inToken);
+    nosql.one(function(token) {
+        if (token.access_token == inToken) {
+            return token;
+        }
+    }, function(err, token) {
+        if (token) {
+            console.log("We found a matching token: %s", inToken);
+        } else {
+            console.log('No matching token was found.');
+        }
+        req.access_token = token;
+        next();
+        return;
+    });
 };
 
 var requireAccessToken = function(req, res, next) {
-	if (req.access_token) {
-		next();
-	} else {
-		res.status(401).end();
-	}
+    if (req.access_token) {
+        next();
+    } else {
+        res.status(401).end();
+    }
 };
 
 var savedWords = [];
 
 app.get('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "read" scope
-	 */
-	res.json({words: savedWords.join(' '), timestamp: Date.now()});
+    if (__.contains(req.access_token.scope, 'read')) {
+
+        res.json({
+            words: savedWords.join(' '),
+            timestamp: Date.now()
+        });
+    } else {
+        res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="read"');
+        res.status(403).end();
+    }
 });
 
 app.post('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "write" scope
-	 */
-	if (req.body.word) {
-		savedWords.push(req.body.word);
-	}
+    if (__.contains(req.access_token.scope, 'write')) {
+
+        if (req.body.word) {
+            savedWords.push(req.body.word);
+        }
+        res.status(201).end();
+    } else {
+        res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="write"');
+        res.status(403).end();
+    }
 });
 
 app.delete('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "delete" scope
-	 */
-	savedWords.pop();
-	res.status(201).end();
+    if (__.contains(req.access_token.scope, 'delete')) {
+        savedWords.pop();
+        res.status(204).end();
+    } else {
+        res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="delete"');
+        res.status(403).end();
+    }
 });
 
-var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+var server = app.listen(9002, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
